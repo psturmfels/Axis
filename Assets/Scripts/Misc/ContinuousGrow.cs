82a3f20b190269eb4e5735a2909da74b5e93a8d1
Pascal Sturmfels <psturm@umich.edu>
2017-10-06 17:42:11 -0400

    added the circle indiactor

---
+++ Assets/Scripts/Misc/ContinuousGrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousGrow : MonoBehaviour {
+	public Vector3 growDirections;
+	public Vector3 growSpeeds;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.localScale += growSpeeds;
+	}
+}
