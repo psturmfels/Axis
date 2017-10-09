878575b23a9a018cbd3a2d7222d05ca11c29b4bd
Pascal Sturmfels <psturm@umich.edu>
2017-10-08 21:23:05 -0400

    added glass shatter enemy death sound effect

--- Assets/Scripts/Contacts/EnemyDie.cs
+++ Assets/Scripts/Contacts/EnemyDie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDie : MonoBehaviour {
+	private string glassBreakBase = "GlassBreak";
 	private SpawnEnemy se; 
 	private int index = -1;
 
@@ -16,13 +17,21 @@ public class EnemyDie : MonoBehaviour {
 	
 	public void Die(Vector3 explosionPosition) {
 		if (index >= 0) {
+			PlayRandomGlassBreak ();
 			TriangleExplosion te = gameObject.AddComponent<TriangleExplosion>();
 			DisableCollider ();
 
 			StartCoroutine(te.SplitMesh(true, explosionPosition));
 
 			se.RegisterDeathAtIndex (index);
-			index = -1;		}
+			index = -1;		
+		}
+	}
+
+	private void PlayRandomGlassBreak() {
+		Debug.Log ("Played glass break.");
+		AudioClip glassBreak = Resources.Load (glassBreakBase + Random.Range (1, 7).ToString ()) as AudioClip;
+		AudioSource.PlayClipAtPoint (glassBreak, Camera.main.transform.position, 0.08f);
 	}
 
 	private void DisableCollider() {
