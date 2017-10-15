ff887b29868c3480aee4b15b744bf346914564b9
psturmfels <psturm@umich.edu>
2017-10-15 13:51:13 -0400

    fixed a bug

--- Assets/Scripts/Misc/SnapToObject.cs
+++ Assets/Scripts/Misc/SnapToObject.cs
@@ -9,6 +9,10 @@ public class SnapToObject : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (target == null) {
+			Destroy (gameObject);
+			return; 
+		}
 		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, -250.0f) + target.transform.up.normalized * localUpOffset + worldOffset;
 	}
 }
